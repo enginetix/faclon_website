edc1570547515304396ab1b7faeb517a6390c622
anuj3918@gmail.com <anuj3918@gmail.com>
2016-03-31 11:48:55 +0530

    Changed Title of dashboard

--- app/js/directives.js
+++ app/js/directives.js
@@ -40,9 +40,9 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 // Default title - load on Dashboard 1
-                var title = 'INSPINIA | Responsive Admin Theme';
+                var title = 'FACLON | Delivering Analytics';
                 // Create your own title pattern
-                if (toState.data && toState.data.pageTitle) title = 'INSPINIA | ' + toState.data.pageTitle;
+                if (toState.data && toState.data.pageTitle) title = 'FACLON | ' + toState.data.pageTitle;
                 $timeout(function() {
                     element.text(title);
                 });
